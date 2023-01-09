346d9f084b3708d7572e7753c24972c7d795ed44
Lauro Otavio Urbano <74082012+laurourbano@users.noreply.github.com>
2023-01-09 13:02:38 -0300

    Update main.ts

--- Desafio OO-typescript/app/main.ts
+++ Desafio OO-typescript/app/main.ts
@@ -1,8 +1,12 @@
+import { aplicacao_1 } from "./aplicacoes/aplicacao_1.js";
+import { aplicacao_2 } from "./aplicacoes/aplicacao_2.js";
+import { aplicacao_3 } from "./aplicacoes/aplicacao_3.js";
+import { aplicacao_4 } from "./aplicacoes/aplicacao_4.js";
 import { aplicacao_5 } from "./aplicacoes/aplicacao_5.js";
 
-//aplicacao_1();
-//aplicacao_2();
-//aplicacao_3();
-//aplicacao_4();
+aplicacao_1();
+aplicacao_2();
+aplicacao_3();
+aplicacao_4();
 aplicacao_5();
 
